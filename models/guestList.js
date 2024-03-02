9689528fa8d635b38704f7d5512f80884501fa9d
rsvincent <rvincent87@gmail.com>
2024-03-02 00:09:11 -0500

    WIP schema

---
+++ models/guestList.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const listSchema = new mongoose.Schema({
+  updatedAt: { type: Date, default: Date.now },
+  contact: [
+    {
+      firstName: { type: String },
+      lastName: { type: String },
+      image: { type: Buffer },
+      question1: { type: String },
+      question2: { type: String },
+      question3: { type: String },
+      invited: { type: Boolean },
+    },
+  ],
+});
+
+module.exports = mongoose.model("guestList", listSchema);
+
+const contactSchema = new mongoose.Schema({
+  firstName: { type: String },
+  lastName: { type: String },
+  image: { type: Buffer },
+  location: {
+    street: {
+      number: { type: Number },
+      name: { type: String },
+      city: { type: String },
+      state: { type: String },
+      postcode: { type: Number },
+      phoneNumber: { type: Number },
+    },
+  },
+});
+
+module.exports = mongoose.model("contactList", contactSchema);
