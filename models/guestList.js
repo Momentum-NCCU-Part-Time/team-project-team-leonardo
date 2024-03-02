1c87862b865fd56f1d418b2e588e99df4e6fffa5
rsvincent <rvincent87@gmail.com>
2024-03-02 10:52:54 -0500

    questions schema added with updates

--- models/guestList.js
+++ models/guestList.js
@@ -7,17 +7,24 @@ const listSchema = new mongoose.Schema({
       firstName: { type: String },
       lastName: { type: String },
       image: { type: Buffer },
-      questions: {
-        question1: { type: String },
-        question2: { type: String },
-        question3: { type: String },
-      },
-      invited: { type: Boolean },
     },
   ],
 });
 
 module.exports = mongoose.model("guestList", listSchema);
+
+const questionsSchema = new mongoose.Schema({
+  updatedAt: { type: Date, default: Date.now },
+  questions: [
+    {
+      question1: { type: String },
+      question2: { type: String },
+      question3: { type: String },
+    },
+  ],
+  invited: { type: Boolean },
+});
+module.exports = mongoose.model("questions", questionsSchema);
 
 const contactSchema = new mongoose.Schema({
   firstName: { type: String },
@@ -33,6 +40,13 @@ const contactSchema = new mongoose.Schema({
       phoneNumber: { type: Number },
     },
   },
+  answers: [
+    {
+      response1: { type: String },
+      response2: { type: String },
+      response3: { type: String },
+    },
+  ],
 });
 
 module.exports = mongoose.model("contactList", contactSchema);
